a0ced5bc6c58e722f9c062d9769ebe75c4cd886d
darcnos <staringeyesencapsulatingtries@gmail.com>
2016-08-30 10:52:21 -0400

    Rewrote somethings for testing, made some things uglier

--- prototype.py
+++ prototype.py
@@ -53,27 +53,34 @@ full_file_path = get_filepaths(filepath)
 i = 0
 
 
+def get_path(i):
+    # Returns the  filename without its extension
+    foo = os.path.splitext(full_file_path[i])[0]
+    head, tail = os.path.split(foo)
+    return head
+
+
 def is_w4(i):
-    # Yields true/false depending on if file is W4
-    foo = full_file_path[i]
+    foo = str(get_path(i))
     level = foo.rsplit('\\')
     bar = str(level[4]).lower()
-    #if 'w4' or 'w-4' or 'w-4s' or "w4's" in str(level[4].lower()):
-    if bar.find("w4") and bar.find("w-4") and bar.find("w-4s") and bar.find("w4's") and bar.find("w-4's") == -1:
-        return False
-    else:
+    if "w4" in bar or "w-4" in bar:
         return True
+    else:
+        return False
 
 
 def is_i9(i):
     # Yields true/false depending on if file is W4
-    foo = full_file_path[i]
+    foo = str(get_path(i))
     level = foo.rsplit('\\')
     bar = str(level[4]).lower()
-    if bar.find("i9") and bar.find("i9's") and bar.find("i9s") and bar.find("i-9") and bar.find("i-9s") and bar.find("i-9's") == -1:
-        return False
-    else:
+    #bar = foo.lower()
+    #if "i9" or "i9's" or "i9s" or "i-9" or "i-9s" or "i-9's" in bar == True:
+    if "i9" in bar or "i-9" in bar:
         return True
+    else:
+        return False
 
 
 def get_emp_num(i):
@@ -82,7 +89,8 @@ def get_emp_num(i):
     empl_num_patter = re.compile('\d{6,14} -')
     if empl_num_patter.search(word) is not None:
         employee_info = re.findall(empl_num_patter, word)
-        return re.findall('\d{6,14}', str(employee_info))
+        boop = re.findall('\d{6,14}', str(employee_info))
+        return boop
     else:
         return '999999'
 
@@ -112,29 +120,40 @@ def is_emp_dir(div_map_input):
 def get_div_map_input(i):
     # Returns what the divider mapping input for a given file should be
     emp_num = str(get_emp_num(i))
-    div_map_path = "\\".join(full_file_path[i].split('\\')[:-1]).lower()
-    foo = div_map_path.rsplit('\\')
-    foo.reverse()
-    itercount = 0
-    for itercount in range(10):
-        div_map_input = foo[itercount]
-        if is_emp_dir(div_map_input) == True or bool(re.search('999999', emp_num)) == True:
-            return 'historical data'
-        else:
-            if divider_lookup_dict.has_key(str(foo[int(itercount)])) == True:
-                div_map_input = str(foo[int(itercount)])
-                return div_map_input
-            else:
-                itercount = itercount + 1
-
-
-def get_div_mapping(i):
-    # Returns the divider mapping based on the divider input
+    dir = get_path(i)
     if is_i9(i) == True:
         return "I-9".lower()
     elif is_w4(i) == True:
         return "W-4-Federal".lower()
-    elif is_emp_dir == True:
+    else:
+        div_map_path = "\\".join(full_file_path[i].split('\\')[:-1]).lower()
+        foo = div_map_path.rsplit('\\')
+        foo.reverse()
+        itercount = 0
+        for itercount in range(10):
+            div_map_input = str(foo[itercount])
+            if bool(re.search('\d{6,14}', div_map_input)) == True or emp_num == '999999':
+                return 'historical data'
+                break
+            else:
+                if divider_lookup_dict.has_key(str(foo[int(itercount)])) == False:
+                    itercount = itercount + 1
+                else:
+                    div_map_input = str(foo[int(itercount)])
+                    return div_map_input
+                    itercount = 10
+                    break
+
+
+def get_div_mapping(i):
+    # Returns the divider mapping based on the divider input
+    emp_num = get_emp_num(i)
+    if is_i9(i) == True:
+        return "I-9".lower()
+    elif is_w4(i) == True:
+        return "W-4-Federal".lower()
+    #elif emp_num == '999999':
+    #elif is_emp_dir(i) == True:
         return 'historical data'
     else:
         div_map_input = get_div_map_input(i)
@@ -146,7 +165,7 @@ def get_sep_mapping(i):
     div_mapping = str(get_div_mapping(i)).lower()
     return str(separator_lookup_dict[str(div_mapping)]).lower()
 
-for i in range(1003):
+for i in range(len(full_file_path)):
     emp_num = str(get_emp_num(i))
     emp_num2 = emp_num.strip("[']")
     fname = str(get_filename(i))
@@ -154,4 +173,7 @@ for i in range(1003):
     sep_map = str(get_sep_mapping(i))
     fpath = str(full_file_path[i])
 
-    print '"' + emp_num2 + '","' + fname + '","' + div_map + '","' + sep_map + '","' +  fpath + '"'
+    if emp_num2 == "999999":
+        i = i + 1
+    else:
+        print '"' + emp_num2 + '","' + fname + '","' + div_map + '","' + sep_map + '","' +  fpath + '"'
